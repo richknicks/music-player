0752ea78848a26f3b6bf660c5f2d7cdbdcef7cd1
Richard Lynch <rlynch2012@gmail.com>
2024-02-17 23:10:53 -0500

    add functionality to progress bar and start stop time

--- script.js
+++ script.js
@@ -1,63 +1,67 @@
-const image = document.querySelector('img');
-const title = document.getElementById('title');
-const artist = document.getElementById('artist');
-const music = document.querySelector('audio');
-const prevBtn = document.getElementById('prev');
-const playBtn = document.getElementById('play');
-const nextBtn = document.getElementById('next');
+const image = document.querySelector("img");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
+const music = document.querySelector("audio");
+const progressContainer = document.getElementById("progress-container");
+const progress = document.getElementById("progress");
+const currentTimeEl = document.getElementById("current-time");
+const durationEl = document.getElementById("duration");
+const prevBtn = document.getElementById("prev");
+const playBtn = document.getElementById("play");
+const nextBtn = document.getElementById("next");
 
 // Music
 const songs = [
-    {
-        name:'jacinto-1',
-        displayName:'Electric Chill Machine',
-        artist:'Jacinto Design',
-    },
-    {
-        name:'jacinto-2',
-        displayName:'Seven Nation Army (Remix)',
-        artist:'Jacinto Design',
-    },
-    {
-        name:'jacinto-3',
-        displayName:'Goodnight, Disco Queen',
-        artist:'Jacinto Design',
-    },
-    {
-        name:'metric-1',
-        displayName:'Front Row (Remix)',
-        artist:'Metric/Jacinto Design',
-    },
-]
+  {
+    name: "jacinto-1",
+    displayName: "Electric Chill Machine",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "jacinto-2",
+    displayName: "Seven Nation Army (Remix)",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "jacinto-3",
+    displayName: "Goodnight, Disco Queen",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "metric-1",
+    displayName: "Front Row (Remix)",
+    artist: "Metric/Jacinto Design",
+  },
+];
 
 // Check if Playing
 let isPlaying = false;
 
-// Play 
+// Play
 function playSong() {
-    isPlaying = true;
-    playBtn.classList.replace('fa-play','fa-pause');
-    playBtn.setAttribute('title', 'Pause');
-    music.play();
+  isPlaying = true;
+  playBtn.classList.replace("fa-play", "fa-pause");
+  playBtn.setAttribute("title", "Pause");
+  music.play();
 }
 
 // Pause
 function pauseSong() {
-    isPlaying = false;
-    playBtn.classList.replace('fa-pause','fa-play');
-    playBtn.setAttribute('title', 'Play');
-    music.pause();
+  isPlaying = false;
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("title", "Play");
+  music.pause();
 }
 
 // Play or Pause Event Listener
-playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
 
 // Update Dom
-function loadSong (song) {
-    title.textContent=song.displayName;
-    artist.textContent=song.artist;
-    music.src = `music/${song.name}.mp3`;
-    image.src = `img/${song.name}.jpg`;
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpg`;
 }
 
 // Current Song
@@ -65,31 +69,60 @@ let songIndex = 0;
 
 // Prev Song
 function prevSong() {
-    songIndex--;
-    if (songIndex < 0) {
-        songIndex = songs.length -1;
-    }
-    console.log(songIndex);
-    loadSong(songs[songIndex]);
-    playSong();
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
 }
 // Next Song
 
 function nextSong() {
-    songIndex++;
-    if (songIndex > songs.length -1) {
-        songIndex = 0;
-    }
-    console.log(songIndex);
-    loadSong(songs[songIndex]);
-    playSong();
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
 }
-
-
 
 // On Load Select First Song
 loadSong(songs[songIndex]);
 
+// Update Progress bar and Time
+function updateProgressBar(e) {
+  if (isPlaying) {
+    const { duration, currentTime } = e.srcElement;
+    console.log(duration, currentTime);
+    // Update Progress Bar width
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+    // Calculate display for duration
+    const durationMinutes = Math.floor(duration / 60);
+    console.log("minutes", durationMinutes);
+    let durationSeconds = Math.floor(duration % 60);
+    if (durationSeconds < 10) {
+      durationSeconds = `0${durationSeconds}`;
+    }
+    console.log("seconds", durationSeconds);
+    // Delay switching duration Element to avoid NaN
+    if (durationSeconds) {
+      durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+    }
+    // Calculate display for current
+    const currentMinutes = Math.floor(currentTime / 60);
+    console.log("minutes", currentMinutes);
+    let currentSeconds = Math.floor(currentTime % 60);
+    if (currentSeconds < 10) {
+      currentSeconds = `0${currentSeconds}`;
+    }
+    console.log("seconds", currentSeconds);
+    currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
+  }
+}
+
 //  Event Listeners
-prevBtn.addEventListener('click', prevSong);
-nextBtn.addEventListener('click', nextSong);
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+music.addEventListener("timeupdate", updateProgressBar);
